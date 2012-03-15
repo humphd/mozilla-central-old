7f517178ab10d3ce5a40ea4ad72d8b74699f6e35
David Humphrey <david.humphrey@senecac.on.ca>
2012-03-14 21:16:41 -0400

    Remove extra blank line

--- content/base/src/nsDocument.h
+++ content/base/src/nsDocument.h
@@ -986,7 +986,6 @@ public:
   // Returns the top element from the full-screen stack.
   Element* FullScreenStackTop();
 
-
   void RequestPointerLock(Element* aElement);
   bool ShouldLockPointer(Element* aElement);
   bool SetPointerLock(Element* aElement, int aCursorStyle);
